7e1d887581794baeb9b138a15ec15dad92ab6142
styUnv <1377590675@qq.com>
2019-02-21 10:34:43 +0800

    update sh

--- init_web.sh
+++ init_web.sh
@@ -1,12 +1,13 @@
 #!/bin/sh 
 
-
 WEB_NAME="GpsDisp"
 PYTHON_IMG_URL="https://pypi.tuna.tsinghua.edu.cn/simple/"
+WEB_BASE_DIR = /home/$WEB_NAME/
+LOGIN_USER= "$(whoami)"
 
 # update permission
-sudo chown -R ubuntu.ubuntu /home/$WEB_NAME/
-sudo chown -R ubuntu.ubuntu /var/log/$WEB_NAME/
+sudo chown -R $LOGIN_USER.$LOGIN_USER $WEB_BASE_DIR
+sudo chown -R $LOGIN_USER.$LOGIN_USER /var/log/$WEB_NAME/
 
 # update computer time zone
 sudo cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
@@ -17,11 +18,11 @@ sudo apt-get -y install python-pip python-dev nginx
 sudo apt-get -y install supervisor
 
 # remove default nginx configuration
-sudo rm -f /etc/nginx/sites-available/default
+sudo rm -f /etc/nginx/sites-enabled/default
 
 # add link this app nginx configuration
-sudo ln -s "$(pwd)""/nginx-hlc.conf" /etc/nginx/conf.d/nginx-hlc.conf
-sudo ln -s "$(pwd)""/supervisor-hlc.conf" /etc/supervisor/conf.d/supervisor-hlc.conf
+sudo ln -s $WEB_BASE_DIR"nginx-hlc.conf" /etc/nginx/conf.d/nginx-hlc.conf
+sudo ln -s $WEB_BASE_DIR"supervisor-hlc.conf" /etc/supervisor/conf.d/supervisor-hlc.conf
 
 # update python image url
 mkdir ~/.pip
@@ -39,15 +40,16 @@ sudo mkdir /var/log/$WEB_NAME
 sudo /home/.pyenvs/$WEB_NAME/bin/pip install -r requestments.txt
 
 # check setting debug
-
-find -name settings.py | xargs perl -pi -e 's|DEBUG = True|DEBUG = False|g'
+find -name $WEB_BASE_DIR/$WEB_NAME/settings.py | xargs perl -pi -e 's|DEBUG = True|DEBUG = False|g'
 
 # open nginx, supervisor
-sudo service nginx start
 sudo service supervisor start
+sudo service nginx start
 
 # open firewall port
 sudo iptables -I INPUT -p tcp --dport 7777 -j ACCEPT
 sudo iptables-save
 
 lsof -i :8999
+
+wget "$(curl ident.me)"":7777/"
